bdd361eec0a63420915c1cb3c2f773647aa067fd
TimmyGray <zuuubada@inbox.ru>
2021-07-13 19:34:11 +0300

    gfg

---
+++ Diet note/Classdata/ActionsClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diet_note
+{
+   public class ActionsClass
+    {
+        
+    }
+}
---
+++ Diet note/Interfaceses/IActions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diet_note
+{
+    public interface IActions
+    {
+        int ChangedID(int id);
+        int ChangedCountOfEat(int countofeat);
+    }
+}
